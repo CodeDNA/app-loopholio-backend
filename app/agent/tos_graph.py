375bd158239cbd5838ab57829201f289e7a99968
codedna <parkerpunj@gmail.com>
2026-08-06 22:26:42 +0530

    Added comments - graph

--- app/agent/tos_graph.py
+++ app/agent/tos_graph.py
@@ -23,6 +23,10 @@ def route_to_clause_extraction(state: ToSAgentState):
     ]
 
 # - - - Agent 1: Clause Extraction Agent - - -
+'''
+## This agent extract legal clauses from the chunks provided.
+## It does not have legal reasoning capability. It only does extraction.
+'''
 async def clause_extraction_node(state: dict) -> ToSAgentState:
     section = state["section"]
     section_title = section["section_title"]
@@ -68,7 +72,21 @@ def route_to_risk_detection(state: ToSAgentState):
         for clause in state["extracted_clauses"]
     ]
 
-# - - - Agent 2: Risk Detection - - -
+#-- - - - Agent 2: Risk Detection - - -
+'''
+## This agent does the core work of this project work i.e. legal reasoning.
+## It analyzes the clauses extracted by the extractuon agent and find legal risks that a user might face.
+
+## The llm call returns:
+DetectedRisks:
+level: RiskLevel        -> Low/Medium/High
+risk_found: bool        -> True/False
+confidence: int         -> How certain it is about the result it produced - 0% - 100%
+category: RiskCategory  -> Category of Risk ex: Privacy, Liability, Payment, etc.
+reason: str             -> Reason why this is a risk
+evidence: str           -> text from the user input that contains this risk
+'''
+
 async def risk_detection_node(state: dict) -> ToSAgentState:
     clause_to_analyze = state["clause"]
     section_title = clause_to_analyze["section_title"]
@@ -125,6 +143,16 @@ def route_to_explainer(state: ToSAgentState):
     ]
 
 # - - - Agent 3: Explainer Agent (with Citations) - - -
+'''
+## This agent is the Explanation Agent.
+## It does not have legal analysis capabilities.
+## It explains legal clause in plain english so that an average user with no legal background can easily understand it.
+
+## The llm call returns:
+ExplainedRisk:
+    whyMatters: str
+    recommendation: str
+'''
 async def explanation_node(state: dict) -> ToSAgentState:
     detected_risk = state["detected_risk"]
     chunk_id = detected_risk["chunk_id"]
@@ -164,6 +192,10 @@ async def explanation_node(state: dict) -> ToSAgentState:
     }
 
 # - - - Agent 4: Final Report Generator - - -
+'''
+## This is an agregator of explanation generated in the previous step.
+## It combines all the explanations stored in the state and then maps it to a 'final_report' that satisfies the front-end contract.
+'''
 def report_generator_node(state: ToSAgentState) -> ToSAgentState:
     # Compiles and finalizes payload structure for the UI contract
     explanations = state["explanations"]
@@ -186,9 +218,28 @@ def report_generator_node(state: ToSAgentState) -> ToSAgentState:
         )
     return {"final_report": formatted_risks}
 
-def build_tos_graph():
-    builder = StateGraph(ToSAgentState)
 
+def build_tos_graph():
+    '''
+    ## The initial state of the graph is:
+        initial_graph_state:
+            "sections": chunks, <- Only this is populated initially
+            "retrieved_chunks": []
+            "extracted_clauses": []
+            "detected_risks": []
+            "plain_explanations": []
+            "final_report": []
+
+    ## Graph Start: The input of the graph is a 'list of chunks' created when the user input(text/docs/pdf) is parsed.
+    ## Each chunk contains a 'section' from the user input.
+    ## For each chunk one 'clause_extraction' node is invoked.
+    ## Each 'clause_extraction' node returns its result to state["extracted_clauses"], later used by 'risk_detection' node.
+    ## Each risk_detection node returns its result to state["detected_risks"], later used by the explainer node.
+    ## Every explainer node add its explanation to the state["explanations"].
+    ## The report_generator node uses the state["explanations"] and generates the final report and return to state["final_report"]
+    '''
+
+    builder = StateGraph(ToSAgentState)
     # Nodes
     builder.add_node("clause_extraction", clause_extraction_node)
     builder.add_node("risk_detection", risk_detection_node)
@@ -209,5 +260,5 @@ def build_tos_graph():
     return builder.compile()
 
 
-# Keeeping it here for now to use with LangGraph Studio compilation
+## Keeeping it here for now to use with LangGraph Studio compilation for quick demo if the UI fails.
 # graph = build_tos_graph()
